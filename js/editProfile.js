a0244e8138dd3f60a08ad717133a8de48d99ead5
GustavBoe <gustavhugoboe@icloud.com>
2025-12-05 09:25:56 +0100

    Created the edit profile function.

---
+++ js/editProfile.js
@@ -0,0 +1,39 @@
+import { get, put } from "./api/apiClient.js";
+
+import { PARAMETER_ID, USER_ENDPOINT } from "./const/const.js";
+
+const profileForm = document.getElementById("profile-form");
+async function fillProfileFields() {
+  const profileResponse = await get(USER_ENDPOINT);
+  const profileData = profileResponse.data;
+  profileForm.avatarURL.value = profileData.avatar.url;
+  profileForm.bannerURL.value = profileData.banner.url;
+  profileForm.bio.value = profileData.bio;
+}
+fillProfileFields();
+profileForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  profileForm.classList = "animate-pulse";
+
+  const formData = new FormData(profileForm);
+  const formFields = Object.fromEntries(formData);
+  formFields.avatar = { url: formFields.avatarURL, alt: "" };
+  formFields.banner = {
+    url: formFields.bannerURL,
+    alt: "",
+  };
+
+  try {
+    const response = await put(USER_ENDPOINT, formFields);
+    alert("Edited profile! Redirecting..");
+    profileForm.classList = "";
+    location.href = `./index.html?id=${PARAMETER_ID}`;
+  } catch (error) {
+    profileForm.classList = "";
+    console.error("Failed to edit profile:", error);
+    alert(`Error: ${error.message}`);
+    location.href = "./index.html";
+  } finally {
+    profileForm.classList = "";
+  }
+});
